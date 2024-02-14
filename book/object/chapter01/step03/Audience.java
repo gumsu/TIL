1ccee09e6a44e5709b39e3408c7eeffe2b35e09a
gumsu <chavi9@naver.com>
2024-02-14 22:23:17 +0900

    오브젝트 챕터1

---
+++ book/object/chapter01/step03/Audience.java
@@ -0,0 +1,18 @@
+package book.object.chapter01.step03;
+
+public class Audience {
+
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+
+    public Long buy(Ticket ticket){
+        return bag.hold(ticket);
+    }
+}
---
+++ book/object/chapter01/step03/TicketSeller.java
@@ -0,0 +1,14 @@
+package book.object.chapter01.step03;
+
+public class TicketSeller {
+
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+    }
+}
